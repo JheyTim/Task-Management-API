8062e284ce8782c6493f0f6b751804fe0924bc18
Timothy Jhey Sarmiento <timothyjhey@gmail.com>
2024-10-31 00:05:03 +0800

    Modified task routes

--- routes/task.js
+++ routes/task.js
@@ -8,18 +8,28 @@ const {
 const authenticateJWT = require('../middleware/authMiddleware');
 const roleMiddleware = require('../middleware/roleMiddleware');
 
-router.post('/tasks', authenticateJWT, roleMiddleware(['admin']), createTask);
-router.get('/tasks', authenticateJWT, roleMiddleware(['admin']), getAllTasks);
+router.post(
+  '/tasks',
+  authenticateJWT,
+  roleMiddleware(['admin', 'user']),
+  createTask
+);
+router.get(
+  '/tasks',
+  authenticateJWT,
+  roleMiddleware(['admin', 'user']),
+  getAllTasks
+);
 router.put(
   '/tasks/:id',
   authenticateJWT,
-  roleMiddleware(['admin']),
+  roleMiddleware(['admin', 'user']),
   updateTask
 );
 router.delete(
   '/tasks/:id',
   authenticateJWT,
-  roleMiddleware(['admin']),
+  roleMiddleware(['admin', 'user']),
   deleteTask
 );
 
